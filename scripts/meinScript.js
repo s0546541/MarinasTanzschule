99b38bfa6630e2e17310c8d3ed536bee535ce0d1
philipplange <philipp.lange23@gmail.com>
2017-11-01 00:54:43 +0100

    + buttons in news + event design prototyp + hovereffekte in nav

--- scripts/meinScript.js
+++ scripts/meinScript.js
@@ -1,19 +1,37 @@
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+console.log("Variablen deklariert");
+
+function myFunction() {
+    console.log("in myFunction");
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+
 $(document).ready(function () {
+
+
+
 
     // News Slider
 
-        $('.news-slider').slick({
-            dots: true,
-            infinite: false,
-            initialSlide: 1,
-            speed: 300,
-            slidesToShow: 1,
-            centerMode: true,
-            variableWidth: true,
-            autoplay: true,
-            autoplaySpeed: 5000,
-            arrows: false
-        });
+    $('.news-slider').slick({
+        dots: true,
+        infinite: false,
+        initialSlide: 1,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        mobileFirst: true
+    });
 
     /*// Ticker
     if ($(".ticker").text().length > 0) {
@@ -29,12 +47,14 @@ $(document).ready(function () {
 
 
 // vTicker Plugin
-   /* $('.ticker').vTicker('init', {
-        speed: 1000,
-        pause: 1000,
-        showItems: 1,
-        padding: 10
-    });
-*/
+    /* $('.ticker').vTicker('init', {
+         speed: 1000,
+         pause: 1000,
+         showItems: 1,
+         padding: 10
+     });
+ */
+
+
 });
 
